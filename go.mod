4819538bc117ab8a257b05c7e7465389c364e106
Rihards Zauls <rihards.zauls@thecatchbox.com>
2020-05-16 18:39:42 +0300

    add basic sqlite connection

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module sensor-api
 
 go 1.14
+
+require github.com/mattn/go-sqlite3 v2.0.3+incompatible
--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"strconv"
+
+	_ "github.com/mattn/go-sqlite3"
+)
 
 func main() {
-	fmt.Println("hello :)");
+	database, _ := sql.Open("sqlite3", "./db/aranet.db")
+	rows, err := database.Query("SELECT sensor_id, serial_code, `name` FROM sensors")
+	if err != nil {
+		panic(err.Error())
+	}
+	var sensorId int
+	var serialCode int
+	var name string
+	for rows.Next() {
+		err := rows.Scan(&sensorId, &serialCode, &name)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		fmt.Println(strconv.Itoa(sensorId) + " (" + strconv.Itoa(serialCode) + "): " + name)
+	}
 }
