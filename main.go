d4146651c42adf448c04a75bc3d806b6433fe31a
Rihards Zauls <rihards.zauls@thecatchbox.com>
2020-05-18 15:45:29 +0300

    minor restructuring

--- main.go
+++ main.go
@@ -3,26 +3,45 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"os"
 	"strconv"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	database, _ := sql.Open("sqlite3", "./db/aranet.db")
-	rows, err := database.Query("SELECT sensor_id, serial_code, `name` FROM sensors")
-	if err != nil {
-		panic(err.Error())
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
 	}
-	var sensorId int
-	var serialCode int
-	var name string
+}
+
+func run() error {
+	db, err := sql.Open("sqlite3", "./db/aranet.db")
+	if err != nil {
+
+		return err
+	}
+	rows, err := db.Query("SELECT sensor_id, serial_code, `name` FROM sensors")
+	if err != nil {
+		return err
+
+	}
+	var (
+		sensorId int
+		serialCode int
+		name string
+	)
+
 	for rows.Next() {
 		err := rows.Scan(&sensorId, &serialCode, &name)
+
 		if err != nil {
-			panic(err.Error())
+			return err
 		}
 
 		fmt.Println(strconv.Itoa(sensorId) + " (" + strconv.Itoa(serialCode) + "): " + name)
 	}
+	return nil
 }
+
